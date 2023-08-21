098e40d789b115c867e103e5cad8dc8406024c27
atomknack <110350925+atomknack@users.noreply.github.com>
2023-08-21 13:59:43 +0400

    update

--- Assets/PlayerScripts/CellPlaceholderStruct.cs
+++ Assets/PlayerScripts/CellPlaceholderStruct.cs
@@ -9,9 +9,30 @@ namespace UKnack.MagicLanjello
         public static CellPlaceholderStruct DefaultPlaceholder =>
             new CellPlaceholderStruct { orientation = 0, cellMesh = 1, material = DEMaterials.DefaultMaterial.id };
 
-    public int orientation;
+        public int orientation;
         public short cellMesh;
         public byte material;
+
+        public CellPlaceholderStruct(int orientation, short cellMesh, byte material)
+        {
+            ValidateParameters(orientation, cellMesh, material);
+            this.orientation = orientation;
+            this.cellMesh = cellMesh;
+            this.material = material;
+        }
+
+        public static bool ValidateParameters(int orientation, short cellMesh, byte material)
+        {
+            if (material < 0 || material >= DEMaterials.Count)
+                throw new Exception($"Material id {material} should be less than {DEMaterials.Count}");
+            //if (DoubleEngine.UHelpers.UThreeDimensionalCellMeshes) helper does not have count
+            if (cellMesh < 0 || cellMesh >= ThreeDimensionalCellMeshes.GetCount())
+                throw new Exception($"Mesh id {cellMesh} should be less than {ThreeDimensionalCellMeshes.GetCount()}");
+            //try{
+            var orient = ScaleInversionPerpendicularRotation3.FromInt(orientation);
+            //}catch (Exception ex) {throw ex}
+            return true;
+        }
 
         public bool Equals(CellPlaceholderStruct other) =>
             orientation == other.orientation &&
--- Assets/PlayerScripts/SOValue_CellPlaceholderStruct.cs
+++ Assets/PlayerScripts/SOValue_CellPlaceholderStruct.cs
@@ -10,7 +10,7 @@ namespace UKnack.Concrete.Values
     public sealed class SOValueMutable_Concrete_CellPlaceholderStruct : SOValueMutable<CellPlaceholderStruct>
     {
         [SerializeField]
-        private USetOrDefault<CellPlaceholderStruct> _value;
+        private USetOrDefault<CellPlaceholderStruct> _value = new (CellPlaceholderStruct.DefaultPlaceholder);
 
         public override CellPlaceholderStruct RawValue { get => _value.Value; protected set => _value.Value = value; }
 
