41ad4ee5cfe9250abce9934cf3ef6f996f0235d4
atomknack <110350925+atomknack@users.noreply.github.com>
2023-08-24 20:03:45 +0400

    Update ResetTRSThenReapplyChildren.cs

--- Assets/magiclanjelloAssembly/Player/ResetTRSThenReapplyChildren.cs
+++ Assets/magiclanjelloAssembly/Player/ResetTRSThenReapplyChildren.cs
@@ -3,19 +3,48 @@ using System.Collections.Generic;
 using UKnack;
 using UKnack.Commands;
 using UnityEngine;
+using Mirror;
 
 public class ResetTRSThenReapplyChildren : CommandMonoBehaviour
 {
     public override void Execute()
     {
-        int childCount = transform.childCount;
-        Vector3[] positions = new Vector3[childCount];
-        Quaternion[] rotations = new Quaternion[childCount];
+        GameObject temp = new GameObject();
+        temp.name = "tempFor_ResetTRSThenReapplyChildren";
 
-        SaveChildRotPos(transform, childCount, positions, rotations);
+        Transform tempTransform = temp.transform;
+        temp.transform.position = transform.position;
+        temp.transform.rotation = transform.rotation;
+        temp.transform.localScale = transform.localScale;
+
+        //int childCount = transform.childCount;
+        //Vector3[] positions = new Vector3[childCount];
+        //Quaternion[] rotations = new Quaternion[childCount];
+
+        //SaveChildRotPos(transform, childCount, positions, rotations);
+        while (transform.childCount > 0)
+        {
+            var item = transform.GetChild(0);
+            //Debug.Log(item.gameObject.name);
+            item.SetParent(tempTransform);
+        }
+
         ResetTransform(transform);
-        RestoreChildRotPos(transform, childCount, positions, rotations);
+
+        //RestoreChildRotPos(transform, childCount, positions, rotations);
+        while (tempTransform.childCount > 0)
+        {
+            var item = tempTransform.GetChild(0);
+            //Debug.Log(item.gameObject.name);
+            item.SetParent(transform);
+        }
+
+
+        Destroy(temp);
+
+
     }
+
 
     private static void SaveChildRotPos(Transform transform, int childCount, Vector3[] positions, Quaternion[] rotations)
     {
