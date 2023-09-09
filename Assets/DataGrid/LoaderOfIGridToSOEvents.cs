b9cb5e8fa75c0170d69b2b7e8a477ed905746774
atomknack <110350925+atomknack@users.noreply.github.com>
2023-09-09 21:08:30 +0400

    FileMenu

---
+++ Assets/DataGrid/LoaderOfIGridToSOEvents.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Mirror;
+using DoubleEngine;
+using DoubleEngine.Atom;
+using UKnack.Events;
+using MagicLanjello.CellPlaceholder;
+
+public class LoaderOfIGridToSOEvents : IThreeDimensionalGrid
+{
+    [SerializeField]
+    private SOPublisher<Vector3Int, CellPlaceholderStruct, NetworkConnectionToClient> _onUpdateCell;
+
+    [SerializeField]
+    private SOPublisher _onClear;
+
+    public void LoadGridFile(string gridFile)
+    {
+        GridLoaders.LoadGrid(this, gridFile);
+    }
+
+    void IThreeDimensionalGrid.UpdateCell(int x, int y, int z, ThreeDimensionalCell cell)
+    {
+        _onUpdateCell.Publish(new Vector3Int(x, y, z), (CellPlaceholderStruct)cell.ToNetGridCell(), null);
+    }
+    void IThreeDimensionalGrid.Clear() 
+    { 
+        _onClear.Publish();
+    }
+
+    ThreeDimensionalCell IThreeDimensionalGrid.GetCell(int x, int y, int z)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    Vec3I IThreeDimensionalGrid.GetDimensions()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+}
